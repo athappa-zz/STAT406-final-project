bee80eb297a97d301893bf95198e941e09feb458
Andrew  Thappa <andrewthappa@Andrews-MacBook-Pro-3.local>
2017-11-07 23:13:40 -0800

    cleaned data

--- code/data-exploration-at.R
+++ code/data-exploration-at.R
@@ -1,23 +1,23 @@
 # Data Exploration
-# Andrew Thappa
 
 rm(list=ls(all=TRUE))
 install.packages("dummies")
 library(dummies)
 
-files.format <- files <- list.files(path = "~/STAT406-final-project/data", 
+data <- "~/STAT406-final-project/data"
+root <- "~/STAT406-final-project/"
+files.format <- files <- list.files(path = data, 
                                     pattern = "*.csv")
 
 for (i in 1:length(files.format)){
   print(files.format[i])
-  files.format[i] = paste("~/STAT406-final-project/data/", files.format[i], sep="")
+  files.format[i] = paste(data, files.format[i], sep="")
 }
 
 
 myfiles <- do.call(rbind, 
                   lapply(files.format, 
                          function(x) read.csv(x, stringsAsFactors = FALSE)))
-names(myfiles)
 
 # keep only a subset of variables
 keep <- c("id", "state", "rural", "sex", "date_of_death", "month_of_death",
@@ -30,28 +30,30 @@ keep <- c("id", "state", "rural", "sex", "date_of_death", "month_of_death",
 
 data.subset <- myfiles[ , which(names(myfiles) %in% keep)]
 
-data.dummies <- cbind(data.subset, dummy(data.subset$state, sep = "_"))
-
+# drop vals if rural > 2
+data.subset <- data.subset[as.numeric(data.subset$rural) == 1 | as.numeric(data.subset$rural) == 2, ]
 dummy.vars <- c("state", "rural", "sex", "treatment_source" , "death_symptoms", "factors_contributing_death",
 "factors_contributing_death_2","religion","marital_status", "highest_qualification","occupation_status",
 "chew", "smoke", "alcohol", "drinking_water_source", "is_water_filter", "toilet_used",
 "household_have_electricity","IsCoveredByHealthScheme")
 
-test <- data.subset[1:100,]
-test <- dummy.data.frame(data.subset, names = dummy.vars, sep = ".")
+clean.data <- dummy.data.frame(data.subset, 
+                               names = dummy.vars, sep = ".")
+# Random subset to test
+# test <- dummy.data.frame(data.subset[sample(nrow(data.subset), 1000),], names = dummy.vars, sep = ".")
 
- 
-summary(data.subset)
+write.table(clean.data, file=paste(root,"clean_data/clean_data.csv", sep = ""))
+
+#################
+## TODO!!!
+#################
+
 # issues with types: need to convert lots of things to numeric
 sapply(data.subset, class)
 names(data.subset)
 
-
-
 cols.num <- c("a","b")
 data.subset[cols.num] <- sapply(data.subset[cols.num],as.numeric)
-
-
 
 head(data.subset[ , sapply(data.subset, is.numeric)])
 
